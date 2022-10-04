524a7922dc999c557a9cde6e9e92bd3e3569ec42
maricarpio <maripacarpio@gmail.com>
2022-10-03 21:48:58 -0600

    Practica semana 2

---
+++ src/main/java/com/practsem2/Practsem2Application.java
@@ -0,0 +1,13 @@
+package com.practsem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practsem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practsem2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practsem2/Practsem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practsem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practsem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
